36def47477d944e648b46329979dfc0e6654dc9b
Tom Forbes <tom@tomforb.es>
2022-06-01 11:55:35 +0000

    {"name":"heifang-2010-part4","version":"2022.6.1.0","file":"heifang-2010-part4-2022.6.1.0.tar.gz","path":"heifang-2010-part4/heifang-2010-part4-2022.6.1.0.tar.gz"}

---
+++ heifang-2010-part4/heifang-2010-part4-2022.6.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Heifang2010Part4
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Heifang2010Part4'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="heifang-2010-part4",
+    version=Heifang2010Part4.__version__,
+    url="https://github.com/apachecn/heifang-2010-part4",
+    author=Heifang2010Part4.__author__,
+    author_email=Heifang2010Part4.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="黑客防线2010合订本（下）第二部分",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "heifang-2010-part4=Heifang2010Part4.__main__:main",
+            "Heifang2010Part4=Heifang2010Part4.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
