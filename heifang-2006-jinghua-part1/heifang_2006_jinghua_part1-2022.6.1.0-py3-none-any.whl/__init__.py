8891e3b837efecb0dad760dd084531936493d4d7
Tom Forbes <tom@tomforb.es>
2022-06-01 10:04:50 +0000

    {"name":"heifang-2006-jinghua-part1","version":"2022.6.1.0","file":"heifang_2006_jinghua_part1-2022.6.1.0-py3-none-any.whl","path":"heifang-2006-jinghua-part1/heifang_2006_jinghua_part1-2022.6.1.0-py3-none-any.whl"}

---
+++ heifang-2006-jinghua-part1/heifang_2006_jinghua_part1-2022.6.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""heifang-2006-jinghua-part1
+https://github.com/apachecn/heifang-2006-jinghua-part1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.6.1.0"
